3e9b8df6d185fb69555c5d87563e815ecc68b97e
Dylan Chen <zilin@singularity-data.com>
2023-12-22 23:00:46 +0800

    add index

--- models/example/table/t.sql
+++ models/example/table/t.sql
@@ -1,1 +1,8 @@
-select 1 as c union select 2 as c
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['c1']},
+      {'columns': ['c2', 'c1']},
+    ]
+)}}
+select 1 as c1, 1 c2 union select 2 as c1, 2 as c2
